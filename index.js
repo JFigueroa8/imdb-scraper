03153643a45dbc63fbaa2f2b7c8ad99afa111cca
Julmar <julmarfigueroa@gmail.com>
2018-07-24 15:02:32 -0400

    Basic express app

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Scraping is Fun!'
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+})
