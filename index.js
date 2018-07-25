5ad82cf42e69ad82d876857acba26cc5cc657a5e
Julmar <julmarfigueroa@gmail.com>
2018-07-25 14:28:12 -0400

    Added search functionality

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 
+const scraper = require('./scraper');
+
 const app = express();
 
 app.get('/', (req, res) => {
   res.json({
     message: 'Scraping is Fun!'
   });
+});
+
+app.get('/search/:title', (req, res) => {
+  scraper
+  .searchMovies(req.params.title)
+  .then(movies => {
+    res.json(movies);
+  })
 });
 
 const port = process.env.PORT || 3000;
